df06a7df9411645f08c976b6c1badddc3a42c010
Ömer Faruk Ayvaz <mrfrkayvaz@gmail.com>
2026-09-12 23:42:56 +0300

    pre prod docker files updated

--- panel-api/app/api/router.py
+++ panel-api/app/api/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .documents import router as documents_router
+from .health import router as health_router
 from .workspaces import router as workspaces_router
 
 api_router = APIRouter()
+api_router.include_router(health_router)
 api_router.include_router(workspaces_router)
 api_router.include_router(documents_router)
